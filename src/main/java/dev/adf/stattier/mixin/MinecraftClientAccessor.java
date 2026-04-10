c45463b81143896cd8b3f396786abf1e7cec993c
HackerADF <rylenpreis09@gmail.com>
2026-04-09 19:45:18 -0600

    feat: port StatTier to MC 1.21.2

--- src/main/java/dev/adf/stattier/mixin/MinecraftClientAccessor.java
+++ src/main/java/dev/adf/stattier/mixin/MinecraftClientAccessor.java
@@ -1,12 +1,12 @@
 package dev.adf.stattier.mixin;
 
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
-import net.minecraft.class_310;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin({class_310.class})
+@Mixin(MinecraftClient.class)
 public interface MinecraftClientAccessor {
-   @Accessor("field_39420")
-   YggdrasilAuthenticationService getAuthenticationService();
+    @Accessor
+    YggdrasilAuthenticationService getAuthenticationService();
 }
